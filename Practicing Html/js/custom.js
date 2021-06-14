52388b0ce549eba4a6c65809955ed8b83f272f47
ArthPatel0311 <81942849+ArthPatel0311@users.noreply.github.com>
2021-06-14 15:29:51 -0400

    changdune

--- Practicing Html/js/custom.js
+++ Practicing Html/js/custom.js
@@ -1,4 +1,4 @@
-particlesJS("partisdcle", {
+particlesJS("partcle", {
   "particles": {
     "number": {
       "value": 400,
@@ -221,6 +221,119 @@ particlesJS("particle2", {
   },
   "retina_detect": true
 });
+
+particlesJS("particle3", {
+  "particles": {
+   "number": {
+     "value": 400,
+     "density": {
+       "enable": true,
+       "value_area": 1000
+       
+     }
+   },
+   "color": {
+     "value": "#ffffff"
+   },
+   "shape": {
+     "type": "circle",
+     "stroke": {
+       "width": 0,
+       "color": "#000000"
+     },
+     "polygon": {
+       "nb_sides": 5
+     },
+     "image": {
+       "src": "img/github.svg",
+       "width": 100,
+       "height": 50
+     }
+   },
+   "opacity": {
+     "value": .4,
+     "random": false,
+     "anim": {
+       "enable": false,
+       "speed": 200,
+       "opacity_min": 0.1,
+       "sync": false
+     }
+   },
+   "size": {
+     "value": 1.5,
+     "random": true,
+     "anim": {
+       "enable": false,
+       "speed": 30,
+       "size_min": 0.1,
+       "sync": false
+     }
+   },
+   "line_linked": {
+     "enable": true,
+     "distance": 75,
+     "color": "#ffffff",
+     "opacity": 0.5,
+     "width": 1
+   },
+   "move": {
+     "enable": true,
+     "speed": 3,
+     "direction": "none",
+     "random": false,
+     "straight": false,
+     "out_mode": "out",
+     "bounce": false,
+     "attract": {
+       "enable": false,
+       "rotateX": 600,
+       "rotateY": 1200
+     }
+   }
+ },
+ "interactivity": {
+   "detect_on": "canvas",
+   "events": {
+     "onhover": {
+       "enable": true,
+       "mode": "grab"
+     },
+     "onclick": {
+       "enable": true,
+       "mode": "push"
+     },
+     "resize": true
+   },
+   "modes": {
+     "grab": {
+       "distance": 75,
+       "line_linked": {
+         "opacity": 1
+       }
+     },
+     "bubble": {
+       "distance": 200,
+       "size": 40,
+       "duration": 2,
+       "opacity": 8,
+       "speed": 3
+     },
+     "repulse": {
+       "distance": 70,
+       "duration": 0.4
+     },
+     "push": {
+       "particles_nb": 4
+     },
+     "remove": {
+       "particles_nb": 3
+     }
+   }
+ },
+ "retina_detect": true
+});
+
 
 
 /* ---- stats.js config ---- */
